69008690d6eec3a819b7ccd9034e33ccd18a4b5f
lovetonight <hoan.khoc@gmail.com>
2024-04-25 10:22:40 +0700

    outlier

--- new-model/detect_outlier.py
+++ new-model/detect_outlier.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 
 
-def read_data():
-    df = pd.read_csv("./data/all_data.csv")
+def read_data(flag):
+    if flag == 0:
+        df = pd.read_csv("./data/full_wallet_data_0.csv")
+    if flag == 1:
+        df = pd.read_csv("./data/full_wallet_data_1.csv")
+    # df = pd.read_csv("./data/all_data.csv")
     df["borrow_per_balance"] = np.where(
         df["balanceInUSD"] == 0, 0, df["borrowInUSD"] / df["balanceInUSD"]
     )
@@ -21,11 +25,11 @@ def read_data():
     # )
     current_timestamp = datetime.now()
     df["createdAt"] = pd.to_datetime(df["createdAt"])
-    df["createdAt"] = current_timestamp - df["createdAt"]
-    min_value = df['createdAt'].min()
-    max_value = df['createdAt'].max()
-    df = df.copy()
-    df['age'] = (df['createdAt'] - min_value) * (1000 - 0) / (max_value - min_value) + 0
+    df["age"] = (current_timestamp - df["createdAt"]).dt.total_seconds()
+    # min_value = df['createdAt'].min()
+    # max_value = df['createdAt'].max()
+    # df = df.copy()
+    # df['age'] = (df['createdAt'] - min_value) * (1000 - 0) / (max_value - min_value) + 0
 
     # Drop column
     df = (
@@ -38,8 +42,8 @@ def read_data():
     return df
 
 
-def read_data_no_outlier():
-    df = read_data()
+def read_data_no_outlier(flag, flag_outlier=None):
+    df = read_data(flag)
     df = df.dropna()
     # Balance outlier
     # Sử dụng z-score 1
@@ -52,10 +56,18 @@ def read_data_no_outlier():
         "deposit_per_balance",
         "borrow_per_deposit",
     ]
-    for label in label_value_in_usd:
-        df["zscore"] = (df[label] - df[label].mean()) / df[label].std()
+    # If flag_outlier is None, calculate outlier with all field
+    if flag_outlier == None:
+        for label in label_value_in_usd:
+
+            df["zscore"] = (df[label] - df[label].mean()) / df[label].std()
+            df = df[df["zscore"] < 1]
+    else:
+        df["zscore"] = (df["balanceInUSD"] - df["balanceInUSD"].mean()) / df[
+            "balanceInUSD"
+        ].std()
         df = df[df["zscore"] < 1]
     return df
-    # Number of - outlier
 
-read_data_no_outlier()
+
+# read_data_no_outlier()
