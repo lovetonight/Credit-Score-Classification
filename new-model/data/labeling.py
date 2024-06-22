04b5ea4b25ecbfa19dbc59d612143ee23647a8bb
lovetonight <hoan.khoc@gmail.com>
2024-06-22 23:35:22 +0700

    update

--- new-model/data/labeling.py
+++ new-model/data/labeling.py
@@ -5,46 +5,58 @@ df = pd.read_csv(file_path)
 
 
 def add_label(df):
+
     for index, row in df.iterrows():
         totalAsset = row["totalAsset"]
         numberOfLiquidation = row["numberOfLiquidation"]
         borrowInUSD = row["borrowInUSD"]
         depositInUSD = row["depositInUSD"]
+
         # Label 4
+
         if (
             totalAsset > 1000000
             and numberOfLiquidation == 0
             and borrowInUSD / totalAsset < 0.2
-            # and borrowInUSD > 0
-            and depositInUSD > 0
+            # # and borrowInUSD > 0
+            # and depositInUSD > 0
         ):
             df.loc[index, "1st_label"] = 4
             df.loc[index, "2nd_label"] = 4
+        elif (
+            totalAsset > 100000
+            and numberOfLiquidation == 0
+            and borrowInUSD == 0
+            and depositInUSD > 0
+        ):
+            df.loc[index, "1st_label"] = 4
+            df.loc[index, "2nd_label"] = 3
 
         # Label 3
-        if (
-            totalAsset > 1000000
+
+        elif (
+            totalAsset > 100000
             and numberOfLiquidation == 0
             and 0.2 <= borrowInUSD / totalAsset < 0.3
         ):
-
             df.loc[index, "1st_label"] = 3
             df.loc[index, "2nd_label"] = 4
-        if (
+
+        elif (
             10000 <= totalAsset < 100000
             and 0 < borrowInUSD / totalAsset < 0.2
             and numberOfLiquidation == 0
         ):
-
             df.loc[index, "1st_label"] = 3
             df.loc[index, "2nd_label"] = 3
 
         # Label 2
-        if 1000 < totalAsset < 10000 and numberOfLiquidation == 0:
 
+        elif 1000 < totalAsset < 10000 and numberOfLiquidation == 0:
             df.loc[index, "1st_label"] = 2
             df.loc[index, "2nd_label"] = 2
-        if (
+
+        elif (
             totalAsset > 10000
             and 0.3 <= borrowInUSD / totalAsset < 0.4
             and numberOfLiquidation == 0
@@ -54,52 +66,69 @@ def add_label(df):
             df.loc[index, "2nd_label"] = 3
 
         # Label 1
-        if (
+
+        elif (
             totalAsset <= 1000
             and 0 <= numberOfLiquidation <= 3
             and depositInUSD > 0
             and borrowInUSD / totalAsset < 0.4
         ):
-
             df.loc[index, "1st_label"] = 1
             df.loc[index, "2nd_label"] = 1
 
-        if totalAsset <= 1000 and 0 <= numberOfLiquidation <= 3 and depositInUSD == 0:
-
+        elif totalAsset <= 1000 and 0 <= numberOfLiquidation <= 3 and depositInUSD == 0:
             df.loc[index, "1st_label"] = 1
             df.loc[index, "2nd_label"] = 0
 
         # if (
+
         #     1000 < totalAsset <= 100000
+
         #     and 0.4 <= borrowInUSD / totalAsset < 0.6
+
         #     and numberOfLiquidation == 1
+
         # ):
+
         #     if (
+
         #         df.loc[index, "1st_label"] is not None
+
         #         and df.loc[index, "2nd_label"] is not None
+
         #     ):
+
         #         tmp = make_dict(
+
         #             df.loc[index, "address"],
+
         #             df.loc[index, "1st_label"],
+
         #             df.loc[index, "2nd_label"],
+
         #             1,
+
         #             2,
+
         #         )
+
         #         result_list.append(tmp)
+
         #     df.loc[index, "1st_label"] = 1
+
         #     df.loc[index, "2nd_label"] = 2
-        if (
+
+        elif (
             totalAsset > 1000
             and numberOfLiquidation > 0
             and 0 < borrowInUSD / totalAsset < 0.4
         ):
-
             df.loc[index, "1st_label"] = 1
             df.loc[index, "2nd_label"] = 0
 
         # Label 0
-        if totalAsset <= 1000 and numberOfLiquidation > 3:
 
+        elif totalAsset <= 1000 and numberOfLiquidation > 3:
             df.loc[index, "1st_label"] = 0
             df.loc[index, "2nd_label"] = 0
         elif (
@@ -109,8 +138,10 @@ def add_label(df):
         ):
 
             df.loc[index, "1st_label"] = 0
+
             df.loc[index, "2nd_label"] = 1
-        if (
+
+        elif (
             totalAsset <= 1000
             and 0 <= numberOfLiquidation <= 3
             and depositInUSD > 0
@@ -118,16 +149,21 @@ def add_label(df):
         ):
 
             df.loc[index, "1st_label"] = 1
+
             df.loc[index, "2nd_label"] = 0
-        if (
+
+        elif (
             1000 < totalAsset <= 100000
             and borrowInUSD / totalAsset >= 0.6
             and numberOfLiquidation > 0
         ):
+
             df.loc[index, "1st_label"] = 0
+
             df.loc[index, "2nd_label"] = 1
 
     df.to_csv("all_data_10_5[2][3].csv", index=False)
 
-
 add_label(df)
+
+
